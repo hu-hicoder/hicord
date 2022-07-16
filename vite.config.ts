b7beadd8d0a93a4324b4e0a121381268eb9775d6
toddlerer <74579078+toddlerer@users.noreply.github.com>
2022-07-16 12:08:32 +0900

    chore: add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    checker({
+      typescript: true,
+      eslint: { lintCommand: 'eslint "./src/**/*.{js,jsx,ts,tsx}" --cache' },
+      overlay: { initialIsOpen: false },
+    }),
+  ],
   build: {
     target: 'esnext',
     polyfillDynamicImport: false,
