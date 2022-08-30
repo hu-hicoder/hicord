4057a2545cb320b6e86fba8c02296002e2f3a4f3
kurotakazuki <kabisakanakabi@gmail.com>
2022-08-30 22:40:11 +0900

    design: layout of toolbars

---
+++ src/components/MainToolbar.tsx
@@ -0,0 +1,11 @@
+const MainToolbar = () => {
+  return (
+    <div class="fixed sm:left-6 sm:bottom-6 left-4 bottom-4 tb-card">
+      <span class="material-symbols-outlined tb-icon">add_reaction</span>
+      <span class="material-symbols-outlined tb-icon tb-icon-on">mic</span>
+      <span class="material-symbols-outlined tb-icon">screen_share</span>
+    </div>
+  )
+}
+
+export default MainToolbar
