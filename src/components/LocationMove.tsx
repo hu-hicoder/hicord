94b9b389f345cb4604264df838ea35801f053e44
kurotakazuki <kabisakanakabi@gmail.com>
2022-09-08 20:05:08 +0900

    feat: location move

---
+++ src/components/LocationMove.tsx
@@ -0,0 +1,87 @@
+import { createSignal } from 'solid-js'
+import { localUserInfo, setLocalUserInfo } from '../utils/user'
+import { sendLocalUserCoordinateToAll } from '../utils/send/sendLocalUserCoordinate'
+import { setListener as setAudioListener } from '../utils/audio'
+import { updateDeg } from '../utils/coordinate'
+
+const options = {
+  enableHighAccuracy: true,
+  maximumAge: 30000,
+  timeout: 27000,
+}
+
+const error = () => {
+  alert('位置情報を利用できません')
+}
+
+const LocationMove = () => {
+  const [getHasLocationMove, setHasLocationMove] = createSignal(false)
+  const [getWatchID, setWatchID] = createSignal<number>()
+  const [getBaseCoord, setBaseCoord] = createSignal<GeolocationCoordinates>()
+  const success = (position: GeolocationPosition) => {
+    const dx = getBaseCoord().latitude - position.coords.latitude
+    const dy = getBaseCoord().longitude - position.coords.longitude
+    console.log(
+      position.coords.latitude,
+      position.coords.longitude,
+      position.coords.heading
+    )
+
+    // set info
+    setLocalUserInfo((preUserInfo) => {
+      return {
+        ...preUserInfo,
+        x: preUserInfo.x + dx * 1000,
+        y: preUserInfo.y + dy * 1000,
+        deg: updateDeg(dx, dy, preUserInfo.deg),
+      }
+    })
+
+    // set audio listener
+    setAudioListener(localUserInfo())
+
+    sendLocalUserCoordinateToAll()
+    // update BaseCoord
+    setBaseCoord(position.coords)
+  }
+
+  const clickLocationMove = () => {
+    if (getHasLocationMove()) {
+      if (getWatchID()) {
+        navigator.geolocation.clearWatch(getWatchID())
+      }
+      setHasLocationMove(false)
+    } else {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          setBaseCoord(position.coords)
+        })
+        // watch position
+        const watchID = navigator.geolocation.watchPosition(
+          success,
+          error,
+          options
+        )
+        setWatchID(watchID)
+        setHasLocationMove(true)
+      } else {
+        console.log('geolocationを取得することができません')
+      }
+    }
+  }
+
+  return (
+    <div>
+      <span
+        class={`material-symbols-outlined tb-item ${
+          getHasLocationMove() ? 'tb-item-on' : ''
+        }`}
+        onClick={clickLocationMove}
+      >
+        person_pin_circle
+      </span>
+    </div>
+  )
+}
+
+export default LocationMove
--- src/utils/coordinate.ts
+++ src/utils/coordinate.ts
@@ -7,3 +7,14 @@ export type Coordinate = {
   y: number
   deg: number
 }
+
+export const updateDeg = (dx: number, dy: number, deg: number) => {
+  if (4 < dx ** 2 + dy ** 2) {
+    let r = Math.atan2(dy, dx)
+    if (r < 0) {
+      r = r + 2 * Math.PI
+    }
+    return Math.floor((r * 360) / (2 * Math.PI)) - 90
+  }
+  return deg
+}
